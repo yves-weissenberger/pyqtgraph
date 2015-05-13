0906109d7fb1159a82145a4f3a69c3e0bc3c6195
Luke Campagnola <luke.campagnola@gmail.com>
2015-05-13 16:31:05 -0400

    Added GroupBox widget with collapse handle Added save/restore methods to ComboBox

--- pyqtgraph/widgets/TableWidget.py
+++ pyqtgraph/widgets/TableWidget.py
@@ -449,7 +449,6 @@ class TableWidgetItem(QtGui.QTableWidgetItem):
             return
         
         try:
-
             self.value = type(self.value)(self.text())
         except ValueError:
             self.value = str(self.text())
